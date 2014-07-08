6f7cda4e598e1531e4d4f874261d835dc842e330
smurphylaw <smurphylaw@gmail.com>
2014-07-07 20:41:58 -0600

    Changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Surfipedia::Application.routes.draw do
   
   devise_for :users
   resources :wikis
--- db/migrate/20140626185753_change_nameto_username.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNametoUsername < ActiveRecord::Migration
-  def change
-    change_column :users, :name, :username
-  end
-end
